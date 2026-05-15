8e7031f1a7ebdf5be5370add3066398942e5384f
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-05-15 13:07:35 -0600

    Implement stack functions in pila.c

--- src/pila.c
+++ src/pila.c
@@ -1,4 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "../include/pila.h"
+#include <stdlib.h>
+#include <string.h>
+
+Pila* crearPila(){
+    Pila* nueva_pila = (Pila*)malloc(sizeof(Pila));
+    if(nueva_pila==NULL){
+        return NULL;
+    } else {
+        nueva_pila->tope = NULL;
+        return nueva_pila;
+    }
+}
+
+void push(Pila* p, void* dato, size_t size){
+    NodoPila* nuevo = (NodoPila*)malloc(sizeof(NodoPila));
+    nuevo->dato = malloc(size);
+    memcpy(nuevo->dato, dato, size);
+    nuevo->siguiente = p->tope;
+    p->tope = nuevo;
+}
+
+void* pop(Pila* p){
+    if(pilaVacia(p)){
+        return NULL;
+    } else {
+        NodoPila* tmp = p->tope;
+        void* valor = tmp->dato;
+        p->tope = p->tope->siguiente;
+        free(tmp);
+        return valor;
+    }
+}
+
+int pilaVacia(Pila* p){
+    if(p->tope==NULL){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void destruirPila(Pila* p){
+    while(pilaVacia(p)==0){
+        void* d = pop(p);
+        free(d);
+    }
+    free(p);
+}
